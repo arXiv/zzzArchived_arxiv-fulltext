ad47f29d50e03172659bebf66d0f8d0a30c93d97
erickpeirson <brp53@cornell.edu>
2018-08-03 07:46:10 -0400

    ARXIVNG-1063 switching to arxiv.base.agent

--- bin/start_agent.py
+++ bin/start_agent.py
@@ -1,22 +1,15 @@
-"""Creates the PDFIsAvailable stream if it does not already exist."""
-import boto3
-from botocore.exceptions import ClientError
-import os
-from fulltext.services import credentials
+"""Run the indexing agent stream processor."""
+from arxiv.base.agent import process_stream
+from fulltext.agent import FulltextRecordProcessor
+from fulltext.factory import create_web_app
+
+
+def start_agent() -> None:
+    """Start the record processor."""
+    app = create_web_app()
+    with app.app_context():
+        process_stream(FulltextRecordProcessor, app.config)
+
 
 if __name__ == '__main__':
-    access_key, secret, token = credentials.get_credentials()
-    endpoint = os.environ.get('KINESIS_ENDPOINT')
-    region = os.environ.get('AWS_REGION', 'us-east-1')
-    verify = os.environ.get('KINESIS_VERIFY') == 'true'
-    stream_name = os.environ.get('STREAM_NAME', 'PDFIsAvailable')
-
-    client = boto3.client('kinesis', region_name=region, endpoint_url=endpoint,
-                          aws_access_key_id=access_key,
-                          aws_secret_access_key=secret,
-                          aws_session_token=token,
-                          verify=verify)
-    try:
-        client.describe_stream(StreamName='PDFIsAvailable')
-    except ClientError:
-        client.create_stream(StreamName='PDFIsAvailable', ShardCount=1)
+    start_agent()
--- fulltext/agent/consumer.py
+++ fulltext/agent/consumer.py
@@ -1,106 +1,31 @@
-"""
-This script is called by the KCL MultiLangDaemon to process Kinesis streams.
+"""Provides a record processor for PDFIsAvailable notifications."""
 
-http://docs.aws.amazon.com/streams/latest/dev/kinesis-record-processor-implementation-app-py.html
-https://github.com/awslabs/amazon-kinesis-client-python/blob/master/samples/sample_kclpy_app.py
-"""
-
-import time
-from arxiv.base import logging
+from typing import Dict, List, Any, Optional
 import json
 import os
-import amazon_kclpy
-from amazon_kclpy import kcl
-from amazon_kclpy.v2 import processor
-from amazon_kclpy.messages import ProcessRecordsInput, ShutdownInput
-from fulltext.services import credentials, extractor
+import time
 
-ARXIV_HOME = 'https://arxiv.org'
+from flask import url_for
+
+from arxiv.base import logging
+from arxiv.base.agent import BaseConsumer
+
+from fulltext.extract import extract_fulltext
 
 logger = logging.getLogger(__name__)
+logger.propagate = False
 
 
-class RecordProcessor(processor.RecordProcessorBase):
-    """
-    Processes records received by the Kinesis consumer.
+class BadMessage(RuntimeError):
+    """A malformed notification was encountered."""
 
-    This class is instantiated when the containing script is run by the
-    MultiLangDaemon. The underlying KCL process handles threading, offset
-    tracking, etc. The KCL guarantees that our :class:`.RecordProcessor` will
-    see each record *at least* once; the checkpoint mechanism gives us a way to
-    further guarantee that we only process each record a single time.
 
-    See the ``consumer.properties`` file for configuration details, including
-    streams.
-    """
+class FulltextRecordProcessor(BaseConsumer):
+    """Consumes ``PDFIsAvailable`` notifications, creates extraction tasks."""
 
-    def __init__(self):
-        """Initialize checkpointing state and retry configuration."""
-        self._SLEEP_SECONDS = 5
-        self._CHECKPOINT_RETRIES = 5
-        self._CHECKPOINT_FREQ = 60
-        self._largest_seq = (None, None)
-        self._largest_sub_seq = None
-        self._last_checkpoint_time = None
-
-    def initialize(self, initialize_input):
-        """Called once by a KCLProcess before any calls to process_records."""
-        self._largest_seq = (None, None)
-        self._last_checkpoint_time = time.time()
-        credentials.get_credentials()
-
-    def checkpoint(self, checkpointer: amazon_kclpy.kcl.Checkpointer,
-                   sequence_number=None,
-                   sub_sequence_number=None) -> None:
-        """Make periodic checkpoints while processing records."""
-        for n in range(0, self._CHECKPOINT_RETRIES):
-            try:
-                checkpointer.checkpoint(sequence_number, sub_sequence_number)
-                return
-            except kcl.CheckpointError as e:
-                if 'ShutdownException' == e.value:
-                    # A ShutdownException indicates that this record processor
-                    #  should be shutdown. This is due to some failover event,
-                    #  e.g. another MultiLangDaemon has taken the lease for
-                    #  this shard.
-                    logger.info("Encountered shutdown exception, skipping"
-                                " checkpoint")
-                    return
-                elif 'ThrottlingException' == e.value:
-                    # A ThrottlingException indicates that one of our
-                    #  dependencies is is over burdened, e.g. too many dynamo
-                    #  writes. We will sleep temporarily to let it recover.
-                    if self._CHECKPOINT_RETRIES - 1 == n:
-                        logger.error("Failed to checkpoint after %i attempts,"
-                                     " giving up." % n)
-                        return
-                    else:
-                        logger.info("Was throttled while checkpointing, will"
-                                    " attempt again in %i seconds"
-                                    % self._SLEEP_SECONDS)
-                elif 'InvalidStateException' == e.value:
-                    logger.error("MultiLangDaemon reported an invalid state"
-                                 " while checkpointing.")
-                else:  # Some other error
-                    logger.error("Encountered an error while checkpointing,"
-                                 " error was %s" % e)
-            time.sleep(self._SLEEP_SECONDS)
-
-    def request_extraction(self, document_id: str) -> None:
-        """Request fulltext extraction via the extraction service API."""
-        try:
-            pdf_url = '%s/pdf/%s' % (ARXIV_HOME, document_id)
-            extractor.extract(document_id, pdf_url)
-        except Exception as e:
-            msg = '%s: failed to extract fulltext: %s' % (document_id, e)
-            logger.error(msg)
-            raise RuntimeError(msg) from e
-        logger.info('%s: successfully extracted fulltext' % document_id)
-
-    def process_record(self, data: bytes, partition_key: bytes,
-                       sequence_number: int, sub_sequence_number: int) -> None:
+    def process_record(self, record: dict) -> None:
         """
-        Called for each record that is passed to process_records.
+        Call for each record that is passed to process_records.
 
         Parameters
         ----------
@@ -108,108 +33,24 @@ class RecordProcessor(processor.RecordProcessorBase):
         partition_key : bytes
         sequence_number : int
         sub_sequence_number : int
+
+        Raises
+        ------
+        IndexingFailed
+            Indexing of the document failed in a way that indicates recovery
+            is unlikely for subsequent papers, or too many individual
+            documents failed.
+
         """
+        time.sleep(self.sleep)
+        logger.debug(f'Processing record {record["SequenceNumber"]}')
         try:
-            deserialized = json.loads(data.decode('utf-8'))
-        except Exception as e:
-            logger.error("Error while deserializing data: %s" % e)
-            logger.error("Data payload: %s" % data)
-            return   # Don't bring down the whole batch.
+            deserialized = json.loads(record['Data'].decode('utf-8'))
+        except json.decoder.JSONDecodeError as e:
+            logger.error("Error while deserializing data %s", e)
+            logger.error("Data payload: %s", record['Data'])
+            raise BadMessage('Could not deserialize payload')
 
-        document_id = deserialized.get('document_id')
-        # try:
-        #     self.events.session.update_or_create(sequence_number,
-        #                                          document_id=document_id)
-        # except IOError as e:
-        #     # If we can't connect, there is no reason to proceed. Make noise.
-        #     msg = "Could not connect to extraction events database: %s" % e
-        #     logger.error(msg)
-        #     raise RuntimeError(msg) from e
-
-        try:
-            self.request_extraction(document_id)
-        except Exception as e:
-            logger.error("Error while processing document: %s" % e)
-            logger.error("Data payload: %s" % data)
-
-    def should_update_sequence(self, sequence_number: int,
-                               sub_sequence_number: int) -> bool:
-        """
-        Determine whether a new larger sequence number is available.
-
-        Parameters
-        ----------
-        sequence_number : int
-        sub_sequence_number : int
-
-        Returns
-        -------
-        bool
-        """
-        return (self._largest_seq == (None, None) or
-                sequence_number > self._largest_seq[0] or
-                (sequence_number == self._largest_seq[0] and
-                 sub_sequence_number > self._largest_seq[1]))
-
-    def process_records(self, records: ProcessRecordsInput) -> None:
-        """
-        Handle a series of records from the stream.
-
-        Called by a KCLProcess with a list of records to be processed and a
-        checkpointer which accepts sequence numbers from the records to
-        indicate where in the byteseam to checkpoint.
-
-        Parameters
-        ----------
-        records : :class:`amazon_kclpy.messages.ProcessRecordsInput`
-        """
-        try:
-            for record in records.records:
-                data = record.binary_data
-                seq = int(record.sequence_number)
-                sub_seq = record.sub_sequence_number
-                key = record.partition_key
-                self.process_record(data, key, seq, sub_seq)
-                if self.should_update_sequence(seq, sub_seq):
-                    self._largest_seq = (seq, sub_seq)
-
-            # Checkpoints every self._CHECKPOINT_FREQ seconds
-            last_check = time.time() - self._last_checkpoint_time
-            if last_check > self._CHECKPOINT_FREQ:
-                self.checkpoint(records.checkpointer,
-                                str(self._largest_seq[0]),
-                                self._largest_seq[1])
-                self._last_checkpoint_time = time.time()
-
-        except Exception as e:
-            logger.error("Encountered an exception while processing records."
-                         " Exception was %s" % e)
-            logger.error("Seq: %i; Sub seq: %i; Key: %s" % (seq, sub_seq, key))
-            logger.error("{}".format(data))
-
-    def shutdown(self, shutdown: ShutdownInput) -> None:
-        """
-        Shut down record processing gracefully, if possible.
-
-        Called by a KCLProcess instance to indicate that this record processor
-        should shutdown. After this is called, there will be no more calls to
-        any other methods of this record processor.
-
-        Parameters
-        ----------
-        shutdown : :class:`amazon_kclpy.messages.ShutdownInput`
-        """
-        try:
-            if shutdown.reason == 'TERMINATE':
-                # **THE RECORD PROCESSOR MUST CHECKPOINT OR THE KCL WILL BE
-                #   UNABLE TO PROGRESS**
-                # Checkpointing with no parameter will checkpoint at the
-                # largest sequence number reached by this processor on this
-                # shard id.
-                logger.info("Was told to terminate, attempting to checkpoint.")
-                self.checkpoint(shutdown.checkpointer, None)
-            else:    # reason == 'ZOMBIE'
-                # **ATTEMPTING TO CHECKPOINT ONCE A LEASE IS LOST WILL FAIL**
-                logger.info("Shutting down due to failover. Won't checkpoint.")
-        except Exception as e:
-            logger.error("Encountered exception while shutting down: %s" % e)
+        arxiv_id: str = deserialized.get('document_id')
+        logger.info(f'Processing notification for {arxiv_id}')
+        extract_fulltext.delay(arxiv_id, url_for('pdf', paper_id=arxiv_id))
