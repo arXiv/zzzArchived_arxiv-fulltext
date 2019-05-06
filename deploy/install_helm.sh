516283846f6671592e469dc85ce7f7fe8bb369a8
erickpeirson <brp53@cornell.edu>
2019-05-06 11:04:38 -0400

    added checksum verification for kubectl/helm downloads
    add md5 check for downloads
    
    minor
    
    minor
    
    fixed md5 for get_helm
    
    fixed md5 for get_helm

--- deploy/install_helm.sh
+++ deploy/install_helm.sh
@@ -11,11 +11,18 @@ DEPLOYMENT_HOSTNAME=${!DEPLOYMENT_HOSTNAME_VAR}
 
 # Install kubectl & Helm
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.9.2/bin/linux/amd64/kubectl
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
+
+md5sum -c ./deploy/bin.md5
+if [[ "$?" != "0" ]]; then
+    echo "One or more binary does not match expected checksum"
+    exit 1
+fi
+
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 echo "Intalled kubectl"
 
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
 chmod 700 get_helm.sh
 sudo ./get_helm.sh -v v2.8.0 || echo "Helm already installed"
 echo "Intalled Helm"
