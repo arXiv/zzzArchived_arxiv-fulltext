a52000ac067324d77e59198c9031d0967ce11d02
erickpeirson <brp53@cornell.edu>
2019-05-21 12:30:58 -0400

    added some additional extraction tests

--- extractor/tests/test_extractor.py
+++ extractor/tests/test_extractor.py
@@ -12,26 +12,29 @@ DOCKER_REGISTRY = os.environ.get('DOCKER_REGISTRY', '')
 class TestExtractorE2E(TestCase):
     """Ensure that the extractor can be built, and performs as expected."""
 
-    def test_extract(self):
-        pdf_filename = '1702.07336.pdf'
+    __test__ = int(bool(os.environ.get('WITH_INTEGRATION', False)))
+
+    @classmethod
+    def setUpClass(cls):
+        basepath, _ = os.path.split(os.path.abspath(__file__))
+        runpath, _ = os.path.split(basepath)
+        build_result = subprocess.run(
+            "docker build %s -f %s/Dockerfile "
+            "-t arxiv/fulltext-extractor:0.3" % (runpath, runpath),
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+        )
+        assert build_result.returncode == 0
+
+    def do_test_extract(self, pdf_filename):
         basepath, _ = os.path.split(os.path.abspath(__file__))
         pdf_path = tempfile.mkdtemp()
         shutil.copyfile(os.path.join(basepath, 'pdfs', pdf_filename),
                         os.path.join(pdf_path, pdf_filename))
         runpath, _ = os.path.split(basepath)
 
-        build_result = subprocess.run(
-            "docker build %s -f %s/Dockerfile -t arxiv/fulltext-extractor:0.3" % (runpath, runpath),
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
-        )
-        self.assertEqual(
-            build_result.returncode,
-            0,
-            "%s\n\n%s" % (build_result.stdout, build_result.stderr)
-        )
-
         extract_result = subprocess.run(
-            "docker run -it -v %s:/pdfs arxiv/fulltext-extractor:0.3 /pdfs/%s" % (pdf_path, pdf_filename),
+            "docker run -it -v %s:/pdfs "
+            "arxiv/fulltext-extractor:0.3 /pdfs/%s" % (pdf_path, pdf_filename),
             stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
         )
         self.assertEqual(
@@ -47,5 +50,36 @@ class TestExtractorE2E(TestCase):
                 content = f.read()
         except Exception as e:
             self.fail('Failed to open output file: %s' % e)
+        return content
 
+    def test_extract_1702_07336(self):
+        content = self.do_test_extract('1702.07336.pdf')
         self.assertGreater(len(content), 50000)
+
+    def test_extract_9912018(self):
+        content = self.do_test_extract('9912018.pdf')
+        self.assertGreater(len(content), 47000)
+
+    def test_extract_1905_02187(self):
+        content = self.do_test_extract('1905.02187.pdf')
+        self.assertGreater(len(content), 43000)
+
+    def test_extract_9108004(self):
+        content = self.do_test_extract('9108004.pdf')
+        self.assertGreater(len(content), 45000)
+
+    def test_extract_9109009(self):
+        content = self.do_test_extract('9109009.pdf')
+        self.assertGreater(len(content), 27000)
+
+    def test_extract_1905_00506(self):
+        content = self.do_test_extract('1905.00506.pdf')
+        self.assertGreater(len(content), 47000)
+
+    def test_extract_1804_08269(self):
+        content = self.do_test_extract('1804.08269.pdf')
+        self.assertGreater(len(content), 47000)
+
+    def test_extract_1512_03905(self):
+        content = self.do_test_extract('1512.03905.pdf')
+        self.assertGreater(len(content), 20000)
